1ca361246daf930925ef357902e14902f67b6c69
Daniel Silva <daniel.sousasilva97@outlook.com>
2023-02-03 06:32:20 -0300

    LargestPrime created

---
+++ src/LargerstPrime.java
@@ -0,0 +1,26 @@
+public class LargerstPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+        System.out.println(getLargestPrime(7));
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                number /= i;
+                i--;
+            }
+        }
+
+        return number;
+    }
+}
